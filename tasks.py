04e42bf6b9e00f3aa73bc2d34088a02358bc013d
Kane Dou <douqilong@gmail.com>
2013-11-04 11:49:08 +0800

    a then sensible invoke script

--- tasks.py
+++ tasks.py
@@ -14,17 +14,30 @@ for role in ROLES:
     PLAYBOOKS[role] = '%s.yml' % role
 
 
+def play(role, *opts):
+    opts_str = ' '.join(opts)
+    inventory = '-i {0}'.format(INVENTORIES[role])
+    cmd = ' '.join([
+        CMD,
+        inventory,
+        opts_str,
+        PLAYBOOKS[role],
+    ])
+    run(cmd, pty=True)
+
+
 @task
-def play(role, sudo=True, *opts):
-    opts = list(opts)
+def play_local(host, role='workstations', sudo=True, opts=None):
+    opts = opts and opts.split(' ') or []
     if sudo:
         opts.append('-K')
-    inv_opt = '-i %s' % INVENTORIES[role]
-    cmd = ' '.join([CMD, inv_opt, ' '.join(opts), PLAYBOOKS[role]])
-    run(cmd)
+    opts.extend([
+        '-c local',
+        '-l {0}'.format(host),
+    ])
+    play(role, *opts)
 
 
 @task
-def play_local(host, role='workstations'):
-    opts = ['-c local', '-l {0}'.format(host),]
-    play(role, True, *opts)
+def play_remote():
+    pass
